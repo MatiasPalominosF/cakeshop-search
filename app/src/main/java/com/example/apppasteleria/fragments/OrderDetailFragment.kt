101ca4caeab3fe90d6cefbe3f9e5dc983d2ba990
Matías <palominos90@gmail.com>
2020-08-11 01:14:36 -0400

    vista detalles ok

--- app/src/main/java/com/example/apppasteleria/fragments/OrderDetailFragment.kt
+++ app/src/main/java/com/example/apppasteleria/fragments/OrderDetailFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.apppasteleria.R
 import com.example.apppasteleria.data.model.Order
+import kotlinx.android.synthetic.main.fragment_order_detail.*
 
 class OrderDetailFragment : Fragment() {
 
@@ -14,18 +18,36 @@ class OrderDetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        requireArguments().let {
+            order = it.getParcelable("order")!!
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        requireArguments().let {
-            order = it.getParcelable("order")!!
-        }
+        val toolbar = activity?.findViewById<androidx.appcompat.widget.Toolbar>(R.id.appbar)
+        val texto = toolbar?.findViewById<TextView>(R.id.txt_saludo)
+        val buscador = toolbar?.findViewById<LinearLayout>(R.id.buscador)
+        val recuadro = toolbar?.findViewById<LinearLayout>(R.id.recuadro)
+        recuadro?.visibility = View.VISIBLE
+        buscador?.visibility = View.GONE
+        texto?.text = "Detalle producto"
         return inflater.inflate(R.layout.fragment_order_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rellenarLayout()
+    }
+
+    private fun rellenarLayout() {
+        Glide.with(requireContext()).load(order.image).into(imageHeader)
+        txtNombrePasteleria.text = order.nameCakeShop
+        txtProducto.text = order.name
+        txtPrecio.text = order.price
+        txtDescripcion.text = order.description
     }
 
 }
